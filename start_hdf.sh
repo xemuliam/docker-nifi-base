8a193781d1d33827bee87891be27d3e03fa8528c
Viacheslav Kalashnikov <xemuliam@users.noreply.github.com>
2016-10-17 13:30:36 +0300

    2.0

--- start_hdf.sh
+++ start_hdf.sh
@@ -3,31 +3,31 @@
 set -e
 
 do_site2site_configure() {
-  sed -i "s/nifi\.ui\.banner\.text=.*/nifi.ui.banner.text=${BANNER_TEXT}/g" ${HDF_HOME}/nifi/conf/nifi.properties
-  sed -i "s/nifi\.remote\.input\.socket\.port=.*/nifi.remote.input.socket.port=1000${MYID}/g" ${HDF_HOME}/nifi/conf/nifi.properties
-  sed -i "s/nifi\.remote\.input\.secure=true/nifi.remote.input.secure=false/g" ${HDF_HOME}/nifi/conf/nifi.properties
+  sed -i "s/nifi\.ui\.banner\.text=.*/nifi.ui.banner.text=${BANNER_TEXT}/g" ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.remote\.input\.socket\.port=.*/nifi.remote.input.socket.port=1010${MYID}/g" ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.remote\.input\.secure=true/nifi.remote.input.secure=false/g" ${HDF_HOME}/conf/nifi.properties
 }
 
 do_cluster_node_configure() {
 # NiFi properties
-  sed -i "s/nifi\.cluster\.protocol\.is\.secure=true/nifi.cluster.protocol.is.secure=false/g" ${HDF_HOME}/nifi/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g" ${HDF_HOME}/nifi/conf/nifi.properties
-#  sed -i "s/nifi\.cluster\.node\.address=.*/nifi.cluster.node.address=${HOSTNAME}/g" ${HDF_HOME}/nifi/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.protocol\.port=.*/nifi.cluster.node.protocol.port=2000${MYID}/g" ${HDF_HOME}/nifi/conf/nifi.properties
-  sed -i "s/nifi\.web\.http\.port=.*/nifi.web.http.port=808${MYID}/g" ${HDF_HOME}/nifi/conf/nifi.properties
-  sed -i "s/nifi\.state\.management\.embedded\.zookeeper\.start=false/nifi.state.management.embedded.zookeeper.start=true/g" ${HDF_HOME}/nifi/conf/nifi.properties
-  sed -i "s/nifi\.zookeeper\.connect\.string=.*/nifi.zookeeper.connect.string=${NODES_LIST}/g" ${HDF_HOME}/nifi/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.protocol\.is\.secure=true/nifi.cluster.protocol.is.secure=false/g" ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g" ${HDF_HOME}/conf/nifi.properties
+#  sed -i "s/nifi\.cluster\.node\.address=.*/nifi.cluster.node.address=${HOSTNAME}/g" ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.node\.protocol\.port=.*/nifi.cluster.node.protocol.port=1020${MYID}/g" ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.web\.http\.port=.*/nifi.web.http.port=808${MYID}/g" ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.state\.management\.embedded\.zookeeper\.start=false/nifi.state.management.embedded.zookeeper.start=true/g" ${HDF_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.zookeeper\.connect\.string=.*/nifi.zookeeper.connect.string=${NODES_LIST}/g" ${HDF_HOME}/conf/nifi.properties
 
 # State management
-  sed -i "s/<property name=\"Connect String\">.*</<property name=\"Connect String\">${NODES_LIST}</g" ${HDF_HOME}/nifi/conf/state-management.xml
+  sed -i "s/<property name=\"Connect String\">.*</<property name=\"Connect String\">${NODES_LIST}</g" ${HDF_HOME}/conf/state-management.xml
 
 # MyId zookeeper
-  mkdir -p ${HDF_HOME}/nifi/state/zookeeper
-  echo ${MYID} > ${HDF_HOME}/nifi/state/zookeeper/myid
+  mkdir -p ${HDF_HOME}/state/zookeeper
+  echo ${MYID} > ${HDF_HOME}/state/zookeeper/myid
 
 # Zookeeper properties
-  sed -i "s/clientPort=.*/clientPort=218${MYID}/g" ${HDF_HOME}/nifi/conf/nifi.properties
-#  sed -i "/^server\.1=/q" ${HDF_HOME}/nifi/conf/zookeeper.properties; sed -i "s/^server\.1=.*/server.1=/g" ${HDF_HOME}/nifi/conf/zookeeper.properties
+  sed -i "s/clientPort=.*/clientPort=218${MYID}/g" ${HDF_HOME}/conf/nifi.properties
+#  sed -i "/^server\.1=/q" ${HDF_HOME}/conf/zookeeper.properties; sed -i "s/^server\.1=.*/server.1=/g" ${HDF_HOME}/conf/zookeeper.properties
 }
 
 do_site2site_configure
@@ -36,5 +36,5 @@ if [ "$INSTANCE_ROLE" == "cluster-node" ]; then
   do_cluster_node_configure
 fi
 
-tail -F ${HDF_HOME}/nifi/logs/nifi-app.log &
-${HDF_HOME}/nifi/bin/nifi.sh run
+tail -F ${HDF_HOME}/logs/nifi-app.log &
+${HDF_HOME}/bin/nifi.sh run
